28008095cc68fe1478db62d0b1a710228ebe1d8f
mengleng <18337952261@163.com>
2021-03-15 14:42:51 +0800

    v2

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x8Bf1dB2753451292A043F67F7630cd5EF26ed4FA";
+export declare const INIT_CODE_HASH = "0x410cce05b2dd54661a98f4072b5f924fd1f0be8b710a0f81e55d6e27a75b8a62";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
