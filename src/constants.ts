d0ee07c34dfbaf5045dc114ac9dd69a7af4a53ec
mengleng <18337952261@163.com>
2021-03-15 13:53:29 +0800

    v2

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x8Bf1dB2753451292A043F67F7630cd5EF26ed4FA'
+
+export const INIT_CODE_HASH = '0x410cce05b2dd54661a98f4072b5f924fd1f0be8b710a0f81e55d6e27a75b8a62'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
